f62bd2d1db700e4c1828b7bcc187d4ad307b9295
Montserrat <mdanatg@gmail.com>
2024-09-01 16:50:44 -0600

    feat: filter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-// App.tsx
 import React, { useState, useEffect, useCallback } from 'react';
 import Table from './stories/organims/Table';
-import {TableColumn } from './stories/organims/Table/types';
+import { TableColumn } from './stories/organims/Table/types';
 import { Badge } from './stories/molecules/Badge';
 import { Button } from './stories/molecules/Button';
 
@@ -16,9 +15,11 @@ type Client = {
 };
 
 const App: React.FC = () => {
-  
   const [data, setData] = useState<Client[]>([]);
-  
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchField, setSearchField] = useState<keyof Client>('name');
+  const [filteredData, setFilteredData] = useState<Client[]>([]);
+
   // Definir las columnas de la tabla
   const columns: TableColumn[] = [
     { key: 'name', label: 'Nombre', sortable: true, filterable: true },
@@ -26,7 +27,7 @@ const App: React.FC = () => {
     { key: 'state', label: 'Estado', sortable: true, filterable: true },
     { key: 'order_number', label: 'No. Pedido', sortable: true, filterable: true },
     { key: 'status', label: 'Estatus', sortable: true, filterable: true },
-    { key: 'actions', label: 'Acciones'}
+    { key: 'actions', label: 'Acciones' }
   ];
 
   const getBadgeColor = (status: string) => {
@@ -42,40 +43,37 @@ const App: React.FC = () => {
     }
   };
 
-  const deleteRecord = useCallback((id: number) => {
-    setData(prevData => prevData.filter(client => client.id !== id));
-  }, [setData]);
-
   // Data fetching
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/clientes.json');
-
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
         const result = await response.json();
 
         const transformedData = result.map((client: Client) => ({
           ...client,
-          status: <div className='flex'>
-            <Badge
-              label={client.status}
-              color={getBadgeColor(client.status)}
-              className="w-full text-center"
-            />
-          </div>,
+          status: (
+            <div className='flex'>
+              <Badge
+                label={client.status}
+                color={getBadgeColor(client.status)}
+                className="w-full text-center"
+              />
+            </div>
+          ),
           actions: (
             <div className='flex'>
               <Button variant='text' color='light' label='View' />
-              <Button variant='text' color='light' label='Delete' onClick={() => deleteRecord(client.id)}/>
+              <Button variant='text' color='light' label='Delete' onClick={() => deleteRecord(client.id)} />
             </div>
           ),
         }));
 
         setData(transformedData);
+        setFilteredData(transformedData); // Inicialmente, los datos filtrados son los mismos que los datos obtenidos
 
       } catch (error) {
         console.log('Failed to fetch data', error);
@@ -85,9 +83,44 @@ const App: React.FC = () => {
     fetchData();
   }, []);
 
+  const deleteRecord = useCallback((id: number) => {
+    setData(prevData => prevData.filter(client => client.id !== id));
+    setFilteredData(prevData => prevData.filter(client => client.id !== id)); // Actualiza también filteredData
+  }, []);
+
+  const handleSearch = () => {
+    const result = data.filter(client =>
+      client[searchField]?.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(result);
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Table data={data} columns={columns} />
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <div className="mb-4 flex gap-4">
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 border border-gray-300 rounded"
+        />
+         <select
+          value={searchField}
+          onChange={(e) => setSearchField(e.target.value as keyof Client)}
+          className="p-2 border border-gray-300 rounded"
+        >
+          <option value="name">Nombre</option>
+          <option value="email">Correo</option>
+          <option value="state">Estado</option>
+          <option value="order_number">No. Pedido</option>
+          <option value="status">Estatus</option>
+        </select>
+        <Button variant='contained' color='primary' label='Buscar' onClick={handleSearch} />
+      </div>
+      <Table data={filteredData} columns={columns} />
+
+      {filteredData.length < 0 && <p>No se encontraron resultados</p> }
     </div>
   );
 };
